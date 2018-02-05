9a0c2d82a3e1a9e6a7018f625b9d16b9906cd968
yushulx <lingxiao1002@gmail.com>
2018-02-05 16:47:49 +0800

    Initial commit

--- docscanner/ViewController.swift
+++ docscanner/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var openVideoViewButton:UIButton!
+    var dcsView:DcsView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dcsView = DcsView.self.init(frame:CGRect.init(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        view.addSubview(dcsView)
+        
+        // Show video view when DcsView is loaded.
+        dcsView.currentView = DVE_VIDEOVIEW
+
+        // Set the video view capture mode.
+        dcsView.videoView.mode = DME_DOCUMENT
+
+        // Set the next view after the cancel or capture button is clicked.
+        dcsView.videoView.nextViewAfterCancel = DVE_IMAGEGALLERYVIEW
+        dcsView.videoView.nextViewAfterCapture = DVE_EDITORVIEW
+
+        // Create a button to show video view
+        openVideoViewButton = UIButton(frame:CGRect(x:self.view.center.x-100, y:self.view.center.y-20, width: 200, height: 40))
+        openVideoViewButton.setTitle("Back to Camera", for: .normal)
+        openVideoViewButton.setTitleColor(UIColor.blue, for: .normal)
+        openVideoViewButton.addTarget(self, action:#selector(onClick), for:UIControlEvents.touchUpInside);
+        dcsView.imageGalleryView.addSubview(openVideoViewButton);
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +41,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @objc func onClick(){
+        dcsView.currentView = DVE_VIDEOVIEW
+    }
 }
 
